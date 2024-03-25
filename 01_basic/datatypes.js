889d465b2b00bd630ef8575e52740d6fb07c38a3
Pri-Nigam <priyanshunigam1607@gmail.com>
2024-03-25 16:57:08 +0530

    25 March(Datatypes)

---
+++ 01_basic/datatypes.js
@@ -0,0 +1,43 @@
+JavaScript has 8 Datatypes
+
+
+1. String
+2. Number
+3. Bigint
+4. Boolean
+5. Undefined               // only declared and not defined.
+6. Null                    // stand alone empty value or lack of any value.
+7. Symbol
+8. Object                  // key:value pairs
+
+
+
+// Numbers:
+let length = 16;
+let weight = 7.5;
+
+// Strings:
+let color = "Yellow";
+let lastName = "Johnson";
+
+// Booleans
+let x = true;
+let y = false;
+
+// Object:
+const person = {firstName:"John", lastName:"Doe"};
+
+// Array object:
+const cars = ["Saab", "Volvo", "BMW"];
+
+// Date object:
+const date = new Date("2022-03-25");
+
+
+let x;       // Now x is undefined
+
+let y = null;   // empty value 
+
+
+
+typeof()      // is used to show the type of data.
