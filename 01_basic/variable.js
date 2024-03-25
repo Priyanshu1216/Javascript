e6c1da238bb492247e337b10ad2785bdff3e82e9
Pri-Nigam <priyanshunigam1607@gmail.com>
2024-03-25 10:44:10 +0530

    25 March(When and which variable to use)

--- 01_basic/variable.js
+++ 01_basic/variable.js
@@ -22,3 +22,17 @@ let ans var both are used to define variables but there is a subtle difference b
 variable defined with keyword var have scope issue (block scope and functional scope) hence they are not used and instead of that let keyword is used to define the variable.
 
 */
+
+
+
+When to Use var, let, or const?
+
+1. Always declare variables
+
+2. Always use const if the value should not be changed
+
+3. Always use const if the type should not be changed (Arrays and Objects)
+
+4. Only use let if you can't use const
+
+5. Only use var if you MUST support old browsers because of scope isSecureContext,(by default var scope is global, so once changed all values of that variable would be changed)
