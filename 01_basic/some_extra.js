b8b2427df13bef0dd8e829bc39bd1d39e2699361
Pri-Nigam <priyanshunigam1607@gmail.com>
2024-03-25 10:41:24 +0530

    25 March(Extra)

---
+++ 01_basic/some_extra.js
@@ -0,0 +1,78 @@
+The JavaScript syntax defines two types of values:
+
+Fixed values
+Variable values
+
+
+Fixed values are called Literals.
+
+Variable values are called Variables.
+
+
+
+// Literals
+
+The two most important syntax rules for fixed values are:
+
+1. Numbers are written with or without decimals: // 10.00, 1001
+
+2. Strings are text, written within double or single quotes: // "Priyanshu"
+
+
+
+Keyword 
+
+Keyword as special words that are reserved by the javascript and can not be used as variables. because there meaning has already been defined.Fixed
+
+for example, let is only used to declare a variable
+
+
+
+
+// JavaScript Identifiers / Names 
+
+
+Identifiers are JavaScript names.
+
+Identifiers are used to name variables and keywords, and functions.
+
+The rules for legal names are the same in most programming languages.
+
+A JavaScript name must begin with:
+
+A letter (A-Z or a-z)
+A dollar sign ($)
+Or an underscore (_)
+
+
+
+All JavaScript identifiers are case sensitive. 
+
+The variables lastName and lastname, are two different variables:
+
+
+
+JavaScript and Camel Case
+Historically, programmers have used different ways of joining multiple words into one variable name:
+
+Hyphens:
+
+first-name, last-name, master-card, inter-city.   //  Hyphens are not allowed in JavaScript. They are reserved for subtractions.
+
+Hyphens are not allowed in JavaScript. They are reserved for subtractions.
+
+Underscore:
+
+first_name, last_name, master_card, inter_city.
+
+Upper Camel Case (Pascal Case):
+
+FirstName, LastName, MasterCard, InterCity.
+
+Lower Camel Case:
+
+JavaScript programmers tend to use camel case that starts with a lowercase letter:
+
+firstName, lastName, masterCard, interCity.
+
+
