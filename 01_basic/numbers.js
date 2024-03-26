f529426ca73b112088b1ecd0c02aa9e2e7b914bc
Pri-Nigam <priyanshunigam1607@gmail.com>
2024-03-27 00:55:05 +0530

    26 March Number functions

---
+++ 01_basic/numbers.js
@@ -0,0 +1,15 @@
+// parseFloat and parseFloat are the two number functions
+
+// parseInt : it conversts any string or float number to integer
+
+console.log(parseInt("32"));
+console.log(parseInt(23.23));
+console.log(parseInt("43ab")); // note if any chanracter is present in stirng but after number then only parseInt will work
+console.log(parseInt("ab43ab")); // will not conver to integer
+
+// parseFloat : it conversts any string or integer number to Float
+
+console.log(parseFloat("32"));
+console.log(parseFloat(23.23));
+console.log(parseFloat("43ab")); // note if any chanracter is present in stirng but after number then only parseFloat will work
+console.log(parseFloat("ab43ab")); // will not conver to integer
