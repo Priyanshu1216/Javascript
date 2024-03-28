bfda05fbfeeb539c78b08eea55b2f4bd41e7cac1
Pri-Nigam <priyanshunigam1607@gmail.com>
2024-03-28 15:00:22 +0530

    28 March Swicth-case(2)

--- 01_basic/swicth_case.js
+++ 01_basic/swicth_case.js
@@ -1,3 +1,61 @@
+// The switch statement is used to perform different actions based on different conditions.
+
+// syntax
+
+// switch(expression) {
+//   case x:
+//     // code block
+//     break;
+//   case y:
+//     // code block
+//     break;
+//   default:
+//     // code block
+// }
+
+
+/*
+
+This is how it works:
+
+The switch expression is evaluated once.
+The value of the expression is compared with the values of each case.
+If there is a match, the associated block of code is executed.
+If there is no match, the default code block is executed.
+
+*/
+
+
+// Example
+
+
+switch (new Date().getDay()) {
+  case 0:
+    day = "Sunday";
+    break;
+  case 1:
+    day = "Monday";
+    break;
+  case 2:
+     day = "Tuesday";
+    break;
+  case 3:
+    day = "Wednesday";
+    break;
+  case 4:
+    day = "Thursday";
+    break;
+  case 5:
+    day = "Friday";
+    break;
+  case 6:
+    day = "Saturday";
+}
+
+
+
+// Another example 
+
 function getLetter(s) {
   let letter;
 
@@ -33,5 +91,5 @@ function getLetter(s) {
 }
 
 
-
+console.log(getLetter('asdfg'))
 
