fe27d5b39ef13d180d281b58f2f7de101e10eb9a
Pri-Nigam <priyanshunigam1607@gmail.com>
2024-03-28 15:57:55 +0530

    28 March Compare function

--- 01_basic/array_functions.js
+++ 01_basic/array_functions.js
@@ -156,6 +156,56 @@ changeToString(arr8);
 
 // second largest
 
+/*
+
+Numeric Sort
+By default, the sort() function sorts values as strings.
+
+This works well for strings ("Apple" comes before "Banana").
+
+If numbers are sorted as strings, "25" is bigger than "100", because "2" is bigger than "1".
+
+Because of this, the sort() method will produce incorrect result when sorting numbers.
+
+You can fix this by providing a compare function:
+
+*/
+
+// Exmple of compare function
+
+const points = [40, 100, 1, 5, 25, 10];
+points.sort(function(a, b){return a - b});
+
+
+// compare function
+
+/*
+
+The Compare Function
+The purpose of the compare function is to define an alternative sort order.
+
+The compare function should return a negative, zero, or positive value, depending on the arguments:
+
+function(a, b){return a - b}
+When the sort() function compares two values, it sends the values to the compare function, and sorts the values according to the returned (negative, zero, positive) value.
+
+If the result is negative, a is sorted before b.
+
+If the result is positive, b is sorted before a.
+
+If the result is 0, no changes are done with the sort order of the two values.
+
+Example:
+
+The compare function compares all the values in the array, two values at a time (a, b).
+
+When comparing 40 and 100, the sort() method calls the compare function(40, 100).
+
+The function calculates 40 - 100 (a - b), and since the result is negative (-60),  the sort function will sort 40 as a value lower than 100.
+
+You can use this code snippet to experiment with numerically and alphabetically sorting:
+
+*/
 
 function getSecondLargest(nums) {
   let secMax;
