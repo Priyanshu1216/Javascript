c20973c8e15f76d73c5175bbd5edf18430896678
Pri-Nigam <priyanshunigam1607@gmail.com>
2024-03-29 00:29:33 +0530

    29 March String interpolation

---
+++ 01_basic/string_interpolation.js
@@ -0,0 +1,26 @@
+// Template Strings use back-ticks (``) rather than the quotes ("") to define a string:
+
+let text = `Hello World!`;
+
+// Template Strings allow both single and double quotes inside a string:
+
+
+let text1 = `He's often called "Johnny"`;
+
+/*
+Interpolation
+Template String provide an easy way to interpolate variables and expressions into strings.
+
+The method is called string interpolation.
+
+The syntax is:
+
+${...}
+
+*/
+
+
+let firstName = "John";
+let lastName = "Doe";
+
+let text2 = `Welcome ${firstName}, ${lastName}!`;
