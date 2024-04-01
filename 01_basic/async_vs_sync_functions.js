2c279df2d61e7363df70c7bff566a6971de25206
Pri-Nigam <priyanshunigam1607@gmail.com>
2024-04-01 15:38:51 +0530

    1 April Async vs Sync functions

---
+++ 01_basic/async_vs_sync_functions.js
@@ -0,0 +1,16 @@
+/* Synchronous function
+
+Together, one after other,sequantial.
+Only one thing is happening at a time.
+
+*/
+
+
+
+/* Asynchronous function
+
+Opposite of synchronous.
+happens in part.
+Multiple things are context switching with each other.
+
+*/
