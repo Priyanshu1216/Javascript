89843492b1156b867fd9d926bff8295923abb86e
Pri-Nigam <priyanshunigam1607@gmail.com>
2024-04-01 18:27:09 +0530

    jest configuration

---
+++ jest.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  // Specify the root directory of your project
+  roots: ['01_basic'],
+
+  // Specify the test environment
+  testEnvironment: 'node',
+
+  // Optionally, you can add more configuration options here
+};
+
