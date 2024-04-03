39625b2d955640d9f886fa278a639a5a33221952
Pri-Nigam <priyanshunigam1607@gmail.com>
2024-04-03 16:09:23 +0530

    03 April MERN Assignment(Palindrome)

---
+++ 01_basic/MERN tests.js/easy/palindrome.js
@@ -0,0 +1,29 @@
+/*
+  Implement a function `isPalindrome` which takes a string as argument and returns true/false as its result.
+  Note: the input string is case-insensitive which means 'Nan' is a palindrom as 'N' and 'n' are considered case-insensitive.
+*/
+
+function isPalindrome(str) {
+  let result = true;
+  str = str.toUpperCase();
+  const symbols =[' ',',','.','?','!'];
+  let str1 = "";
+  for (let i = 0; i < str.length; i++){
+    if (symbols.includes(str[i])){
+      continue;
+    }
+    else{
+      str1 += str[i];
+    }
+  }
+  newStr = str1.split('').reverse().join('')
+  if (str1 == newStr){
+    result = true;
+  }
+  else{
+    result = false;
+  }
+  return result;
+}
+
+module.exports = isPalindrome;
