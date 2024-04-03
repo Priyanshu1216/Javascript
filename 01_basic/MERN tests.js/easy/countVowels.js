be46aae0caee5c1b45b21c436d5a1d98cdb769a8
Pri-Nigam <priyanshunigam1607@gmail.com>
2024-04-03 15:30:14 +0530

    04 April MERN Assignment(Count vowels)

---
+++ 01_basic/MERN tests.js/easy/countVowels.js
@@ -0,0 +1,19 @@
+/*
+  Implement a function `countVowels` that takes a string as an argument and returns the number of vowels in the string.
+  Note: Consider both uppercase and lowercase vowels ('a', 'e', 'i', 'o', 'u').
+
+  Once you've implemented the logic, test your code by running
+*/
+
+function countVowels(str) {
+  let vowel = ['A','E','I','O','U','a','e','i','o','u']
+  let count = 0;
+  for (let i = 0; i < str.length; i++){
+    if(vowel.includes(str[i])){
+      count += 1;
+    }
+  }
+  return count;
+}
+
+module.exports = countVowels;
